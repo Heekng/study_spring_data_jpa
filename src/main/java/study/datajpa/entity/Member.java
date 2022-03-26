9132b6292ea6dc21f36e405b243562b26c627445
heekng <rhkd1769@gmail.com>
2022-03-27 02:49:16 +0900

    예제 도메인 모델과 동작확인

--- src/main/java/study/datajpa/entity/Member.java
+++ src/main/java/study/datajpa/entity/Member.java
@@ -1,24 +1,42 @@
 package study.datajpa.entity;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username", "age"}) //team은 무한루프를 방지하기 위해 작성하지 않음
 public class Member {
 
     @Id
     @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
     private String username;
+    private int age;
 
-    protected Member() {;} //jpa가 프록시 기술을 사용할 때 (하이버네이트 등 ..) 프라이빗으로 막아두면 막혀버림
+    @ManyToOne(fetch = FetchType.LAZY) //member을 조회할 때에는 member만 조회, team은 가짜 객체로 가지고 잇다가 team을 사용할 때 team을 쿼리 조회
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+//    protected Member() {;} //jpa가 프록시 기술을 사용할 때 (하이버네이트 등 ..) 프라이빗으로 막아두면 막혀버림
 
     public Member(String username) {
         this.username = username;
+    }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if (team != null) {
+            changeTeam(team);
+        }
+    }
+
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
     }
 }
---
+++ src/test/java/study/datajpa/entity/MemberTest.java
@@ -0,0 +1,57 @@
+package study.datajpa.entity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberTest {
+
+    @PersistenceContext
+    EntityManager em;
+
+    @Test
+    public void testEntity() throws Exception {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 10, teamA);
+        Member member3 = new Member("member3", 10, teamB);
+        Member member4 = new Member("member4", 10, teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        //초기화
+        em.flush();
+        em.clear();
+        // persist를 하면 jpa 영속성 컨텍스트에 모아둠
+        // flush를 하면 강제로 DB에 insert쿼리를 날림
+        // clear을 하면 jpa 에 영속성 컨텍스트에 있는 것을 다 날린다.
+
+        //확인
+        List<Member> members = em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+
+        for (Member member : members) {
+            System.out.println("member = " + member);
+            System.out.println("-> member.team = " + member.getTeam());
+        }
+
+
+    }
+
+}
